05249aa2b0d623ab55e33f8954735a7532353bbb
yousuke86 <yousuke7201@yahoo.co.jp>
2020-04-27 09:58:11 +0900

    make-message-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :group_users
-  has_many :groups, through: :group_users   
+  has_many :groups, through: :group_users  
+  has_many :messages
+  
   validates :name, presence: true, uniqueness: true
 
 end
