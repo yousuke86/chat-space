59737d0f629135497aea136afc40910b12c50961
yousuke86 <yousuke7201@yahoo.co.jp>
2020-05-04 01:44:27 +0900

    add_asynchronous_communication

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,14 +3,23 @@ $(function(){
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action');
-    
     $.ajax({
       url: url,
-      type: 'POST',
+      type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.main-chat__messages').append(html);
+      $('form')[0].reset();
+      $('.send-btn').prop('disabled',false);
+      $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   });
 });
